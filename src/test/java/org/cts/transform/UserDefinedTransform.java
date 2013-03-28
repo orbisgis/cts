ccf435aac4d6a91c47e98d589853494a143a220e
Erwan Bocher <erwan.bocher@ec-nantes.fr>
2013-03-28 17:53:00 +0100

    Add new user test to manage french projections Add log4j file

--- src/test/java/org/cts/transform/UserDefinedTransform.java
+++ src/test/java/org/cts/transform/UserDefinedTransform.java
@@ -35,12 +35,15 @@ import java.util.HashMap;
 import java.util.Map;
 import org.cts.CoordinateOperation;
 import org.cts.Ellipsoid;
+import org.cts.Identifier;
 import org.cts.Parameter;
+import org.cts.op.CoordinateOperationSequence;
 import org.cts.op.LongitudeRotation;
 import org.cts.op.UnitConversion;
 import org.cts.op.projection.LambertConicConformal1SP;
 import org.cts.units.Measure;
 import org.cts.units.Unit;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -55,19 +58,55 @@ public class UserDefinedTransform extends BaseCoordinateTransformTest {
     @Test
     public void fromLambert2EtenduToWGS84() throws Exception {
         double[] pointSource = new double[]{584173.736, 2594514.828};
-        double[] pointDest = new double[]{2.114551393, 50.345609791};
+        double[] pointDest = new double[]{50.345609791, 2.114551393};
         CoordinateOperation LAMBERT2E = LambertConicConformal1SP.LAMBERT2E.inverse();
         double[] result = LAMBERT2E.transform(pointSource);
         result = LongitudeRotation.PARIS2GREENWICH.transform(result);
         CoordinateOperation conv = UnitConversion.RAD2DD;
         result = conv.transform(result);
-        System.out.println(coord2string(result));
-        checkEquals("Lambert 2 to WGS84", result, pointDest, 0.01);
+        assertTrue(checkEquals("Lambert 2 to WGS84 degrees", result, pointDest, 0.01));
+    }
+
+    @Test
+    public void fromLambert1ToGeographic() throws Exception {
+        double[] pointSource = new double[]{1029705.083, 272723.849};
+        Map<String, Measure> params1 = new HashMap<String, Measure>();
+        params1.put(Parameter.LATITUDE_OF_ORIGIN,
+                new Measure(55, Unit.GRAD));
+        params1.put(Parameter.CENTRAL_MERIDIAN,
+                new Measure(0.0, Unit.GRAD));
+        params1.put(Parameter.SCALE_FACTOR, new Measure(0.999877340,
+                Unit.UNIT));
+        params1.put(Parameter.FALSE_EASTING, new Measure(600000.0,
+                Unit.METER));
+        params1.put(Parameter.FALSE_NORTHING, new Measure(200000.0,
+                Unit.METER));
+        LambertConicConformal1SP LAMBERT1 = new LambertConicConformal1SP(
+                Ellipsoid.CLARKE1880IGN, params1);
+        CoordinateOperation LAMBERT1_I = LAMBERT1.inverse();
+        double[] result = LAMBERT1_I.transform(pointSource);
+        result = LongitudeRotation.PARIS2GREENWICH.transform(result);
+        assertTrue(checkEquals("Lambert I to Geographic in radians", result, new double[]{
+                    0.87266462600, 0.14551209931}, 1E-11));
+    }
+
+    @Test
+    public void fromLambert1ToLambert2Etendu() throws Exception {
+        double[] pointSource = new double[]{750000.00, 300000.00};
+        
+        CoordinateOperation LAMBERT2E = LambertConicConformal1SP.LAMBERT2E;        
+        CoordinateOperation LAMBERT1 = LambertConicConformal1SP.LAMBERT1.inverse();
+        
+        final CoordinateOperation op = new CoordinateOperationSequence(new Identifier("CTS", "L1 -> L2E",
+						"From Lambert 1 to Lambert 2 etendu"),  LAMBERT1, LAMBERT2E);
+        double[] result = op.transform(pointSource);
+        assertTrue(checkEquals("Lambert I to Lambert 2 étendu", result,
+                new double[]{750283.12, 2600360.77}, 0.01));
 
     }
 
     @Test
-    public void frenchProjections() throws Exception {
+    public void fromGeographicToLambert1() throws Exception {
         Map<String, Measure> params1 = new HashMap<String, Measure>();
         params1.put(Parameter.LATITUDE_OF_ORIGIN,
                 new Measure(55, Unit.GRAD));
@@ -81,56 +120,10 @@ public class UserDefinedTransform extends BaseCoordinateTransformTest {
                 Unit.METER));
         final LambertConicConformal1SP LAMBERT1 = new LambertConicConformal1SP(
                 Ellipsoid.CLARKE1880IGN, params1);
-        LOGGER.info(LAMBERT1.toString());
-        final CoordinateOperation LAMBERT1_I = LAMBERT1.inverse();
-        LOGGER.info(LAMBERT1_I.toString());
-
-        Map<String, Measure> params2 = new HashMap<String, Measure>();
-        params2.put(Parameter.LATITUDE_OF_ORIGIN, new Measure(52.0,
-                Unit.GRAD));
-        params2.put(Parameter.CENTRAL_MERIDIAN,
-                new Measure(0.0, Unit.GRAD));
-        params2.put(Parameter.SCALE_FACTOR, new Measure(0.99987742,
-                Unit.UNIT));
-        params2.put(Parameter.FALSE_EASTING, new Measure(600000.0,
-                Unit.METER));
-        params2.put(Parameter.FALSE_NORTHING, new Measure(2200000.0,
-                Unit.METER));
-        final LambertConicConformal1SP LAMBERT2E = new LambertConicConformal1SP(
-                Ellipsoid.CLARKE1880IGN, params2);
-        LOGGER.info(LAMBERT2E.toString());
-
-        // Application num�rique prise sur
-        // http://professionnels.ign.fr/DISPLAY/000/526/700/5267002/transformaton.pdf
-        double[] cc = new double[]{750000.00, 300000.00};
-        LOGGER.info("Lambert 1 : " + coord2string(cc));
-        LOGGER.info("Geographiques : "
-                + coord2string(LAMBERT1_I.transform(cc)));
-        cc = LAMBERT2E.transform(cc);
-        LOGGER.info("Lambert 2 étendu : " + coord2string(cc));
-        checkEquals("test Lambert I vers Lambert 2 étendu", cc,
-                new double[]{750283.12, 2600360.77}, 0.01);
-
-        LOGGER.info("");
-        // Application
-        cc = new double[]{1029705.083, 272723.849};
-        LOGGER.info("Lambert 1 : " + coord2string(cc));
-        cc = LAMBERT1_I.transform(cc);
-        cc = LongitudeRotation.PARIS2GREENWICH.transform(cc);
-        LOGGER.info(LongitudeRotation.PARIS2GREENWICH.toString());
-        LOGGER.info("Geographiques : " + coord2string(cc));
-        checkEquals("Lambert I vers Géographique", cc, new double[]{
-                    0.87266462600, 0.14551209931}, 1E-11);
-
-        LOGGER.info("");
-        // Application
-        cc = new double[]{0.87266462600, 0.14551209931};
-        LOGGER.info("Géographiques : " + coord2string(cc));
-        cc = LongitudeRotation.GREENWICH2PARIS.transform(cc);
-        cc = LAMBERT1.transform(cc);
-        LOGGER.info("Lambert 1 : " + coord2string(cc));
-        checkEquals("Lambert I vers Géographique", cc, new double[]{
+        double[] pointSource = new double[]{0.87266462600, 0.14551209931};
+        double[] result = LongitudeRotation.GREENWICH2PARIS.transform(pointSource);
+        result = LAMBERT1.transform(result);
+        checkEquals("From Geographic radians to Lambert 1", result, new double[]{
                     1029705.083, 272723.849}, 1E-3);
-
     }
 }
