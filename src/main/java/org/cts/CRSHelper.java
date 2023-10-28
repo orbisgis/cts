652ab185d13eeec63ea0bea883ff1b03f5870937
ebocher <erwan.bocher@cnrs.fr>
2023-10-28 17:59:42 +0200

    Fix 149 No projection for elements in the epsg register that have ellipsoid: +ellps=aust_SA

--- src/main/java/org/cts/CRSHelper.java
+++ src/main/java/org/cts/CRSHelper.java
@@ -630,7 +630,7 @@ public class CRSHelper {
         Ellipsoid ellps = null;
 
         if (null != ellipsoidName) {
-            ellps = Ellipsoid.ellipsoidFromName.get(ellipsoidName.replaceAll("[^a-zA-Z0-9_]", "").toLowerCase());
+            ellps = Ellipsoid.ellipsoidFromName.get(ellipsoidName.replaceAll("[^a-zA-Z0-9]", "").toLowerCase());
         }
         if (ellps == null && authorityCode != null) {
             String[] authNameWithKey = authorityCode.split(":");
